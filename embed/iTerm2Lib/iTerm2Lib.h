506a321e2d60d221648c1442d509a91425d01310
Jay Koutavas <jay@heynow.com>
2019-09-25 21:36:41 -0400

    First pass at doing an embedded iTerm2, based off lemonmojo's PR #366 (https://github.com/gnachman/iTerm2/pull/366). A key difference between lemonmojo's approach and this approach is instead of adding a iTerm2Lib target to the iTerm2 project, I've created an iTerm2Lib project and referenced iTerm2 as a sub-project, thus not requiring any modification to the iTerm2 project.

---
+++ embed/iTerm2Lib/iTerm2Lib.h
@@ -0,0 +1,8 @@
+#import <Cocoa/Cocoa.h>
+
+#import "iTermLibApplication.h"
+#import "iTermLibController.h"
+#import "iTermLibSessionController.h"
+#import "SessionView+iTermLib.h"
+#import "NSWindow+iTermLib.h"
+
